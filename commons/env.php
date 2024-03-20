d74b837cad740402134acbab55e46789e6096262
Phuong Chu <phuongcdph32281@fpt.edu.vn>
2024-03-20 08:17:00 +0700

    first commit

---
+++ commons/env.php
@@ -0,0 +1,5 @@
+<?php
+// khai báo các biến môi trường , dùng Global
+
+define('PATH_CONTROLLER',__DIR__.'/../controllers'); // DIR đường dẫn đến file chứ file hiện tại
+echo __DIR__;die;
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+// Require các file có trong commons
+
+require_once 'commons/env.php';
+require_once 'commons/helper.php';
+require_once 'commons/connect-db.php';
+
+// Require các file có trong commons
+
+
+// Điều hướng
+
+require_once 'commons/disconnect-db.php';
+
+?>
